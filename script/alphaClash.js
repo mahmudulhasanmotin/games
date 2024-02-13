7414422edb486b478942f7cccb4e20aeb0d2be56
Mahmudul Hasan Motin <mahmudulhasanmotin@gmail.com>
2024-02-14 01:11:46 +0600

    all html code compleate

---
+++ script/alphaClash.js
@@ -0,0 +1,7 @@
+function play(){
+    const homePage = document.getElementById('home-screen')
+    homePage.style.display='none'
+
+    const playPage = document.getElementById('play-ground')
+    playPage.style.display='block'
+}
