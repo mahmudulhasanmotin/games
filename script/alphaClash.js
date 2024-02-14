93761d511fa1a6ae2f2b80155669f1d8b37a50f2
Mahmudul Hasan Motin <mahmudulhasanmotin@gmail.com>
2024-02-15 02:18:21 +0600

    game create compleate

--- script/alphaClash.js
+++ script/alphaClash.js
@@ -8,17 +8,76 @@ function start(){
 
 function continueGame(){
     const alphabet = getRandomAlphabet();
-    console.log('your alphabet',alphabet);
 
     const currentAlphabetElement = document.getElementById('current-alphabet')
     currentAlphabetElement.innerText = alphabet;
 
     setBackgroundColorById(alphabet);
-    // removeBackgroundColorById(alphabet);
 }
 
-
 function play(){
-    start()
-    continueGame()
+    hideElementById ('home-screen');
+    hideElementById ('final-score');
+    showElementById ('play-ground');
+
+    setTextElementValueById('current-life',5)
+    setTextElementValueById('current-score',0)
+
+    continueGame();
+    const hideScreen = document.getElementById('final-score');
+    hideScreen.style.display = 'none';
+}
+
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const exceptedAlphabet = currentAlphabet.toLowerCase();
+
+    if(playerPressed === exceptedAlphabet ){
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+
+        const newScore = currentScore+1;
+        currentScoreElement.innerText = newScore;
+
+        removeBackgroundColorById(exceptedAlphabet);
+        continueGame();
+    }
+    else{
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        const newLife = currentLife - 1;
+
+        currentLifeElement.innerText= newLife;
+        if( newLife === 0){
+            gameOver()
+            finalScorePage()
+
+        }
+
+    }
+
+}
+document.addEventListener('keyup',handleKeyboardButtonPress);
+
+
+
+function gameOver(){
+    hideElementById ('play-ground');
+    showElementById ('final-score');
+
+    const lastScore = getTextElementValueById('current-score')
+    setTextElementValueById('last-score',lastScore);
+
+    const currentAlphabet =getElementTextById('current-alphabet')
+    removeBackgroundColorById(currentAlphabet);
+}
+function finalScorePage(){
+    const finalScorePageShow = document.getElementById('final-score')
+    finalScorePageShow.style.display='block';
 }
--- script/utility.js
+++ script/utility.js
@@ -1,14 +1,11 @@
 function getRandomAlphabet(){
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
-    // console.log(alphabets);
 
     const randomNumber = Math.random() * 25;
     const index = Math.round(randomNumber);
-    // console.log(index);
 
     const alphabet = alphabets[index];
-    // console.log(index,alphabet)
     return alphabet;
 }
 
@@ -20,4 +17,28 @@ function setBackgroundColorById (elementId){
 function removeBackgroundColorById (elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('bg-orange-500')
+}
+
+function hideElementById (elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden')
+}
+function showElementById (elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden')
+}
+function setTextElementValueById( elementId,value){
+    const element = document.getElementById(elementId);
+    element.innerText =value;
+}
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
 }
